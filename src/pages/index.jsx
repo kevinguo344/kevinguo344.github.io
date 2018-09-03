912c6eba8e3de4300216e0226129aa351b82d0d5
Kevin Guo <kg344@cornell.edu>
2018-09-03 01:52:56 -0400

    more relevant components and stuff

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Link from "gatsby-link";
+import PageView from "../components/page-view";
+import Styles from "../style/styles.scss";
 
 export default () =>
-	<div>
+	<PageView>
 		<h1>Hi, I'm Kevin</h1>
-		<p>Here's what I build</p>
+		<h1>This is my website</h1>
+		<h1>I'm a designer, programmer, and engineer</h1>
+		<h1>My goal is to break UX from the screen...</h1>
+		<h1>...and into the world.</h1>
+		<h1>I'm interested in the intersection of the physical and the digital, in the confluence of design, technology, and things that don't mix with either.</h1>
+		<h1>I want to build the novel</h1>
 		<div>
-			<Link to="/projects">Link</Link>
+			<Link to="/projects">See my Projects</Link>
 		</div>
-		<div>
-			<Link to="/counter/">Counter</Link>
-		</div>
-	</div>
+	</PageView>
