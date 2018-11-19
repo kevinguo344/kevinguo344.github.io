1ea923bc54526d2bb5a71381e893d05d197966df
Kevin Guo <kg344@cornell.edu>
2018-11-19 17:40:32 -0500

    added stuff

--- src/components/titlePiece.jsx
+++ src/components/titlePiece.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
-import {Row, Column} from './skeleton.jsx';
+import {Column} from './skeleton.jsx';
+import "./style.css";
 
 export class Tile extends React.Component{
 	constructor(props){
 		super(props);
 		this.title = this.props.title;
+		this.subtitle = this.props.subtitle;
 		this.side = this.props.side;
-		this.image = this.props.image;
 	}
 	render(){
-		
 		return (
-			<Row>
-				<Column>
-
-				</Column>
-			</Row>
+			<Column width="6">
+				<div className={"titleCard " + this.side}>
+					<div className="titleContainer">
+						<h2>{this.title}</h2>
+						<h5>{this.subtitle}</h5>
+					</div>
+					<div className="background"></div>
+				</div>
+			</Column>
 		)
 	}
 }
+
+export default Tile;
