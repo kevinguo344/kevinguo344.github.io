388bf3251bcebe8543cd34b139ef996316ffb470
Kevin Guo <kg344@cornell.edu>
2018-11-19 11:21:42 -0500

    removed margin top

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {Row} from './components/row.js';
 import {Column} from './components/column.js';
 import {Container} from './components/container.js';
 import Markdown from 'markdown-to-jsx';
-
 const mdFile = require('./markdown/test.md');
 
 class App extends Component {
@@ -11,16 +10,13 @@ class App extends Component {
 		return (
 			<Container>
 				<Row>
-					<Column width = "6" style={{"margin-top": "25%"}}>
+					<Column width = "6">
 						<h1><strong>Hi, I'm Kevin</strong></h1>
 						<p>I'm currently a student Cornell University studying Information Science, Systems, & Technology.</p>
 						<p>Next year, I will be pursuing a Masters of Engineering in Mechanical Engineering focusing on Architectural Robotics.</p>
 						<h5><strong>I want to make the built environment responsive through technology.</strong></h5>
 					</Column>
 				</Row>
-				<Markdown>
-					{mdFile}
-				</Markdown>
 			</Container>
 		);
 	}
