e5daa7af08ff090b1282d7c271a4d38300045036
Kevin Guo <kg344@cornell.edu>
2019-01-22 09:38:44 -0500

    created build file

---
+++ build/precache-manifest.58c5d36311e633b2c586bfdff8d92a31.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "76784da018059911b339fc8723efd37d",
+    "url": "/static/media/full-view.76784da0.svg"
+  },
+  {
+    "revision": "d02d39c3a2c5d070e1e9",
+    "url": "/static/css/main.298bae78.chunk.css"
+  },
+  {
+    "revision": "e1edf6fa141e0f9568a7",
+    "url": "/static/js/1.e1edf6fa.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d735447f271548406f2bc774336466d8",
+    "url": "/static/media/designscene.d735447f.JPG"
+  },
+  {
+    "revision": "7b0274c25721df8fa428473e1faf4a00",
+    "url": "/static/media/screenshot.7b0274c2.png"
+  },
+  {
+    "revision": "92ac1883b2761be6e14d05b48de71e34",
+    "url": "/static/media/workflow.92ac1883.png"
+  },
+  {
+    "revision": "0c7c5e76a391366376d698feeb2727e6",
+    "url": "/static/media/stack.0c7c5e76.svg"
+  },
+  {
+    "revision": "1b3953e11119afe7e1ec30d63fb51761",
+    "url": "/static/media/tree.1b3953e1.svg"
+  },
+  {
+    "revision": "23743f56e8d0d388179fa8c3b457786c",
+    "url": "/static/media/ring.23743f56.svg"
+  },
+  {
+    "revision": "258a638cb6326474a180eded3a7d18e2",
+    "url": "/static/media/roma_labels.258a638c.png"
+  },
+  {
+    "revision": "207bfff31855342ca70bd22a23f75214",
+    "url": "/static/media/entire_set.207bfff3.png"
+  },
+  {
+    "revision": "3993eceaac56589fd342c94b59065c1d",
+    "url": "/static/media/icons.3993ecea.svg"
+  },
+  {
+    "revision": "9b8cb16c090de1cf53fd3906a3c5a2cf",
+    "url": "/static/media/ar_interaction.9b8cb16c.jpg"
+  },
+  {
+    "revision": "034a750037dbc8edd445a8e6811c3894",
+    "url": "/static/media/team.034a7500.jpg"
+  },
+  {
+    "revision": "e3c7114fdc2e3912430a8a76a362e012",
+    "url": "/static/media/crochetmatic.e3c7114f.png"
+  },
+  {
+    "revision": "d02d39c3a2c5d070e1e9",
+    "url": "/static/js/main.d02d39c3.chunk.js"
+  },
+  {
+    "revision": "6c3dfa0da1a9a9f62e999389f9bbc994",
+    "url": "/static/media/voxel-single.6c3dfa0d.jpg"
+  },
+  {
+    "revision": "83933b43a150f4c5ef6fae9b14dede83",
+    "url": "/static/media/basic.83933b43.png"
+  },
+  {
+    "revision": "4f8888e8bdde5562860e76a8cf891866",
+    "url": "/static/media/voxel-double.4f8888e8.jpg"
+  },
+  {
+    "revision": "1229a3d119c054a20aa78ee3933c3e37",
+    "url": "/static/media/junction.1229a3d1.png"
+  },
+  {
+    "revision": "edb51949a3b0bd00dd41be096e5dd67c",
+    "url": "/static/media/single_layer.edb51949.gif"
+  },
+  {
+    "revision": "bdf4c92f0dd21cdc62c49bd517dbb047",
+    "url": "/static/media/multiple_layers.bdf4c92f.gif"
+  },
+  {
+    "revision": "4c16cf1825ef681ef0d906dacce91086",
+    "url": "/static/media/real_prism.4c16cf18.jpg"
+  },
+  {
+    "revision": "2cf7d002a8d22ec7d4c834941a1c87a4",
+    "url": "/static/media/computer_sim.2cf7d002.gif"
+  },
+  {
+    "revision": "964d09582f7f4e68d6ec522aec7323c5",
+    "url": "/static/media/first_version.964d0958.svg"
+  },
+  {
+    "revision": "11019e1b04e2c5a2592c2e7f808c376c",
+    "url": "/static/media/next_version.11019e1b.svg"
+  },
+  {
+    "revision": "5c8de141e665b046883dc36a5923b9f4",
+    "url": "/static/media/final_version.5c8de141.svg"
+  },
+  {
+    "revision": "c2db0d235095ea7faeecc6684ba6e45b",
+    "url": "/static/media/loop-by-hand.c2db0d23.gif"
+  },
+  {
+    "revision": "10dad6011e44916846cd1de5db4f6ab7",
+    "url": "/static/media/steps.10dad601.svg"
+  },
+  {
+    "revision": "12096cd6d7a3e94a72cd8163e68df6af",
+    "url": "/static/media/scarf.12096cd6.png"
+  },
+  {
+    "revision": "4b2b2d68dd2e0e69ed6ca6557bcdf28f",
+    "url": "/static/media/transFORM.4b2b2d68.jpg"
+  },
+  {
+    "revision": "23b674ff70225cf08f5ff1dcbe53f3ad",
+    "url": "/index.html"
+  }
+];
