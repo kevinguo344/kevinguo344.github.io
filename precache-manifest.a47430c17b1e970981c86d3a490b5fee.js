cbb0b844e8d76012ef4b60eecb22fc7aca93c556
Kevin Guo <kg344@cornell.edu>
2020-05-01 02:31:05 -0400

    Updates

---
+++ precache-manifest.a47430c17b1e970981c86d3a490b5fee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db4cbc6ceada99134723c036f98d332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "305d38856b9c03f71fc0",
+    "url": "/static/css/main.018a873c.chunk.css"
+  },
+  {
+    "revision": "e8b0bf99b191466fa0f7",
+    "url": "/static/js/2.1b7711e0.chunk.js"
+  },
+  {
+    "revision": "6450ede7a7762456ceef7d180049e519",
+    "url": "/static/js/2.1b7711e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "305d38856b9c03f71fc0",
+    "url": "/static/js/main.b062c1e5.chunk.js"
+  },
+  {
+    "revision": "8da9c2f04b175d8f54cc",
+    "url": "/static/js/runtime-main.c23cd7b0.js"
+  },
+  {
+    "revision": "c36e9407e03247b84ccc63d8003b9a5e",
+    "url": "/static/media/cera.c36e9407.md"
+  },
+  {
+    "revision": "b3517cbd41694cdf1161574b2c60153a",
+    "url": "/static/media/communit.b3517cbd.md"
+  },
+  {
+    "revision": "a3149cb52d6290775db94ce50005bb67",
+    "url": "/static/media/crochetmatic.a3149cb5.md"
+  },
+  {
+    "revision": "586a1b061244879d173edf5600336348",
+    "url": "/static/media/distopia.586a1b06.md"
+  },
+  {
+    "revision": "39ac0c87e7bdb9f749c51f12f8cbd83d",
+    "url": "/static/media/ori.39ac0c87.md"
+  },
+  {
+    "revision": "42ee4164df2e4a9f41ffea7ac0e50899",
+    "url": "/static/media/roma.42ee4164.md"
+  }
+]);
